d9dc503421a79a1727b4303c1d8aaf956db7bb58
Christopher Miller <christopher@transcriptic.com>
2017-05-18 18:12:49 -0700

    Separated the Javascript code from the html

---
+++ assets/javascript/game.js
@@ -0,0 +1,105 @@
+$( document ).ready(function() {
+
+  var randNumber = Math.floor(Math.random() * 50);
+  var imageOneValue = Math.floor(Math.random() * 10);
+  var imageTwoValue = Math.floor(Math.random() * 10);
+  var imageThreeValue = Math.floor(Math.random() * 10);
+  var imageFourValue = Math.floor(Math.random() * 10);
+  var wins = 0;
+  var total = 0;
+  var losses = 0;
+  var currentValue = randNumber;
+  console.log(currentValue);
+  $("#current_value").html(randNumber);
+
+  function resetValues() {
+    randNumber = Math.floor(Math.random() * 50);
+    imageOneValue = Math.floor(Math.random() * 10);
+    imageTwoValue = Math.floor(Math.random() * 10);
+    imageThreeValue = Math.floor(Math.random() * 10);
+    imageFourValue = Math.floor(Math.random() * 10);
+    total = 0;
+    currentValue = randNumber;
+    $("#current_value").html(randNumber);
+  };
+
+  if (total < currentValue) {
+    console.log(currentValue)
+    $('#winOrLose').html("Keep Trying!");
+    $("#image_1").click(function() {
+      total += imageOneValue;
+      if (total === currentValue ) {
+        $('#winOrLose').html("You Win!");
+        wins += 1;
+        $('#wins').html(wins);
+        resetValues();
+      } 
+      if (total > currentValue) {
+        $('#winOrLose').html("Sorry, You Lose!");
+        resetValues();
+        losses += 1
+        $('#losses').html(losses);
+      }
+      else {
+        $('#total_score').html(total);
+      }
+    });
+
+    $("#image_2").click(function() {
+      total += imageTwoValue;
+      if (total === currentValue ) {
+        $('#winOrLose').html("You Win!");
+        resetValues();
+        wins += 1;
+        $('#wins').html(wins);
+      } 
+      if (total > currentValue) {
+        $('#winOrLose').html("Sorry, You Lose!");
+        resetValues();
+        losses += 1
+        $('#losses').html(losses);
+      }
+      else {
+        $('#total_score').html(total);
+      }
+    });
+
+    $("#image_3").click(function() {
+      total += imageThreeValue;
+      if (total === currentValue ) {
+        $('#winOrLose').html("You Win!");
+        resetValues();
+        wins += 1;
+        $('#wins').html(wins);
+      } 
+      if (total > currentValue) {
+        $('#winOrLose').html("Sorry, You Lose!");
+        resetValues();
+        losses += 1
+        $('#losses').html(losses);
+      }
+      else {
+        $('#total_score').html(total);
+      }
+    });
+
+    $("#image_4").click(function() {
+      total += imageFourValue;
+      if (total === currentValue ) {
+        $('#winOrLose').html("You Win!");
+        resetValues();
+        wins += 1;
+        $('#wins').html(wins);
+      } 
+      if (total > currentValue) {
+        $('#winOrLose').html("Sorry, You Lose!");
+        resetValues();
+        losses += 1
+        $('#losses').html(losses);
+      }
+      else {
+        $('#total_score').html(total);
+      }
+    });
+  };
+});            
